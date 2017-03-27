39082bd25d194a6e23c3c1103d169f5515566ce3
SanketPatel <sanketpatel.1805@gmail.com>
2017-03-27 00:02:45 -0700

    Make Fuchu work - add program.fs

--- FSharp/DynamicProgramming/MaximizeStockProfit.fs
+++ FSharp/DynamicProgramming/MaximizeStockProfit.fs
@@ -1,11 +1,13 @@
 ﻿module MaximizeStockProfit
 
+// Greedy algorithm : Find the optimal solution 'so far'.
+
 open System
 open NUnit.Framework
 open FsUnit
 open Fuchu
 
-let maxProfit1 p =
+let maxProfitRecursive p =
     
     let rec loop minSoFar maxProfitSoFar rest =
         match rest with
@@ -22,33 +24,34 @@ let maxProfit1 p =
     | [] | [_] -> failwith "Not enough datapoints"
     | h :: t -> loop h Int32.MinValue t
 
-[<TestFixture>]
-type Tests () =
+let maxProfitIterative prices =
 
-    [<Test>]
-    member x.``Basic Tests`` () =
-        [ ([2;3], 1); ([3;2], -1); ([5;3;8;0], 5); ([5;4;3;2;1], -1); ([1;2;3;4;3;2;1], 3) ]
-        |> List.iter (fun (i, o) -> maxProfit1 i |> should equal o)
+    if (List.length prices < 2) then
+        failwith "Not enough datapoints"
 
-    [<Test>]
-    member x.``Negative Tests`` () =
-        [ []; [2] ]
-        |> List.iter
-            (fun (i) ->
-                (fun () -> maxProfit1 i |> ignore)
-                |> should (throwWithMessage "Not enough datapoints") typeof<Exception>)
+    let mutable minPrice  = Int32.MaxValue
+    let mutable maxProfit = Int32.MinValue
+
+    for p in prices do
+        // iterative solution requires mutability.
+        // And problem with mutability - order of these statements matters!
+        maxProfit <- max maxProfit (p - minPrice)
+        minPrice  <- min minPrice p
+
+    maxProfit
 
 [<Tests>]
 let tests =
 
     let expect input output _ =
-        maxProfit1 input |> should equal output
+        maxProfitIterative input |> should equal output
 
     let basicTest i (input, output) =
         testCase (sprintf "%i" i) <| expect input output
 
     let shouldFail input _ =
-        maxProfit1 input |> should (throwWithMessage "Not enough datapoints") typeof<Exception>
+        (fun () -> maxProfitIterative input |> ignore)
+        |> should (throwWithMessage "Not enough datapoints") typeof<Exception>
 
     let negativeTest i input =
         testCase (sprintf "%i" i) <| shouldFail input
@@ -63,3 +66,21 @@ let tests =
             |> List.mapi negativeTest
         )
     ]
+
+(*
+[<TestFixture>]
+type Tests () =
+
+    [<Test>]
+    member x.``Basic Tests`` () =
+        [ ([2;3], 1); ([0;-1], -1); ([5;3;8;0], 5); ([5;4;3;2;1], -1); ([1;2;3;4;3;2;1], 3) ]
+        |> List.iter (fun (i, o) -> maxProfitIterative i |> should equal o)
+
+    [<Test>]
+    member x.``Negative Tests`` () =
+        [ []; [2] ]
+        |> List.iter
+            (fun (i) ->
+                (fun () -> maxProfitIterative i |> ignore)
+                |> should (throwWithMessage "Not enough datapoints") typeof<Exception>)
+*)
